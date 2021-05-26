10848f9730b176fb528511422fe9e91d806cba0f
devansh-dvj <devansh.dvj@gmail.com>
2021-05-26 18:41:53 +0530

    Added command line argument to gif maker

--- results/gif_maker.py
+++ results/gif_maker.py
@@ -1,9 +1,16 @@
 import glob
+import sys
+
 from PIL import Image
 
+# check sys argument
+if len(sys.argv) == 1:
+    print('Usage: python gif_maker.py <file_name: files are of form file_name_*.png>')
+    exit(1)
+
 # filepaths
-fp_in = "flame_5_*.png"
-fp_out = "flame_5.gif"
+fp_in = '{}_*.png'.format(sys.argv[1])
+fp_out = '{}.gif'.format(sys.argv[1])
 
 # https://pillow.readthedocs.io/en/stable/handbook/image-file-formats.html#gif
 img, *imgs = [Image.open(f) for f in sorted(glob.glob(fp_in))]
